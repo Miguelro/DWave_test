537027ba56a007552ac3e84e0090def169c6d556
Miguelro <miguel.rodriguezasensio@gmail.com>
2021-08-14 15:44:52 +0000

    First tirals

---
+++ 2D _checkerboard.py
@@ -0,0 +1,18 @@
+import dimod
+
+#Example took from here: https://quantumcomputing.stackexchange.com/questions/1451/how-do-you-write-a-simple-program-for-a-d-wave-device
+J = {(0,1): 1, (0,2): 1, (1,3): 1, (2,3): 1} #Links
+h = [-1,0,0,0] #Bias
+
+exactsolver = dimod.ExactSolver()
+
+bqm = dimod.BinaryQuadraticModel.from_ising(h,J,offset=-1.0)
+results = exactsolver.sample(bqm)
+
+#print the results
+print(results)
+
+print(bqm.energy({0: 1, 1: -1, 2:-1,3:-1}))
+print(bqm.energy({0: 1, 1: -1, 2:-1,3:1}))
+
+
